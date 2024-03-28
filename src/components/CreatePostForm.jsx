a08f29ab51b891fd021f33a80f4d1ed232e6e791
BC <b.cacsire8@gmail.com>
2024-03-28 16:32:27 -0400

    used state to traeck form changes

--- src/components/CreatePostForm.jsx
+++ src/components/CreatePostForm.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function CreatePostForm() {
+  const [user, setUser] = useState("");
+  const [content, setContent] = useState("");
+  const [date, setDate] = useState("mm/dd/yyyy");
+
+  let handleUserChange = (event) => {
+    setUser(event.target.value);
+  }
+  let handleContentChange = (event) => {
+    setContent(event.target.value);
+  }
+  let handleDateChange = (event) => {
+    setDate(event.target.value);
+  }
+
+  let handleSubmit = (event) => {
+    setUser("");
+    setContent("");
+    setDate("mm/dd/yyyy");
+  }
 
   return (
     <div>
       <h2>Create Post</h2>
-      <form>
-        <input type="text" placeholder="Name"></input>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" onChange={handleUserChange} value={user}></input>
         <br/>
-        <textarea placeholder="Type your thoughts..."></textarea>
+        <textarea placeholder="Type your thoughts..." onChange={handleContentChange} value={content}></textarea>
         <br/>
-        <input type="date"></input>
-        <button type="submit">Post</button>
+        <input type="date" onChange={handleDateChange} value={date}></input>
+        <button type="submit" disabled={user=="" || content=="" || date=="mm/dd/yyyy"}>Post</button>
       </form>
     </div>
   )
